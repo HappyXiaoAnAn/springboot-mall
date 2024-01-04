31ac3614bedd58c6f1e77c75177c9c0ffb46437c
Happy小安安 <andy85728@gmail.com>
2024-01-04 22:17:11 +0800

    feat: pagination

--- src/main/java/org/happyxiaoanan/springbootmall/controller/ProductController.java
+++ src/main/java/org/happyxiaoanan/springbootmall/controller/ProductController.java
@@ -1,6 +1,8 @@
 package org.happyxiaoanan.springbootmall.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.websocket.server.PathParam;
 import org.happyxiaoanan.springbootmall.constant.ProductCategory;
 import org.happyxiaoanan.springbootmall.dto.ProductQueryParams;
@@ -10,10 +12,12 @@ import org.happyxiaoanan.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -28,13 +32,19 @@ public class ProductController {
 
             // Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
