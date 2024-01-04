2cb59f7156708f56ac42042e63017d22ed280bdd
Happy小安安 <andy85728@gmail.com>
2024-01-04 20:42:26 +0800

    feat: add function getProducts(basic)

--- src/main/java/org/happyxiaoanan/springbootmall/dao/ProductDao.java
+++ src/main/java/org/happyxiaoanan/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package org.happyxiaoanan.springbootmall.dao;
 import org.happyxiaoanan.springbootmall.dto.ProductRequest;
 import org.happyxiaoanan.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/org/happyxiaoanan/springbootmall/service/ProductService.java
+++ src/main/java/org/happyxiaoanan/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package org.happyxiaoanan.springbootmall.service;
 import org.happyxiaoanan.springbootmall.dto.ProductRequest;
 import org.happyxiaoanan.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
