6ae999d11c2533a221d87326c1d252fb63bf143a
Happy小安安 <andy85728@gmail.com>
2024-01-04 16:40:33 +0800

    refactor: use Enum class category

--- src/main/java/org/happyxiaoanan/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/org/happyxiaoanan/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package org.happyxiaoanan.springbootmall.rowmapper;
 
+import org.happyxiaoanan.springbootmall.constant.ProductCategory;
 import org.happyxiaoanan.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,12 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+//        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
