b3c165c036010b26196018d2b2c2896301ec0d08
Happy小安安 <andy85728@gmail.com>
2024-01-04 10:13:29 +0800

    Initialize Spring Boot project

---
+++ src/test/java/org/happyxiaoanan/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.happyxiaoanan.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
