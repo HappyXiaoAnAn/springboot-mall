21c6c81b304184548e62f26615cc2fa425aafe9e
Happy小安安 <andy85728@gmail.com>
2024-01-05 21:25:22 +0800

    feat: check if user exit & update product stock

--- src/main/java/org/happyxiaoanan/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/org/happyxiaoanan/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package org.happyxiaoanan.springbootmall.service.impl;
 
 import org.happyxiaoanan.springbootmall.dao.OrderDao;
 import org.happyxiaoanan.springbootmall.dao.ProductDao;
+import org.happyxiaoanan.springbootmall.dao.UserDao;
 import org.happyxiaoanan.springbootmall.dto.BuyItem;
 import org.happyxiaoanan.springbootmall.dto.CreateOrderRequest;
 import org.happyxiaoanan.springbootmall.model.Order;
 import org.happyxiaoanan.springbootmall.model.OrderItem;
 import org.happyxiaoanan.springbootmall.model.Product;
+import org.happyxiaoanan.springbootmall.model.User;
 import org.happyxiaoanan.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +24,16 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final static Logger log = LoggerFactory.getLogger(OrderService.class);
+
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -38,11 +49,32 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // check if user exit
+        User user = userDao.getUserById(userId);
+
+        if (user == null) {
+            log.warn("UserId: {} not exit!", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // check if product stock is enough
+            if (product == null) {
+                log.warn("Product: {} not exit!", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } else if (product.getStock() < buyItem.getQuantity()) {
+                log.warn("Product: {} is not enough. Remain: {}, Request: {}",
+                        product.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            // update product stock
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
 
             // calculate total price
             int amount = buyItem.getQuantity() * product.getPrice();
