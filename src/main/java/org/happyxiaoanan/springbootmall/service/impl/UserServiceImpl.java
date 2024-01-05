df2cc3e082f2b8e6e18c9c16b64606bed08e8ac5
Happy小安安 <andy85728@gmail.com>
2024-01-05 10:39:46 +0800

    feat: check email & ignore password

--- src/main/java/org/happyxiaoanan/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/org/happyxiaoanan/springbootmall/service/impl/UserServiceImpl.java
@@ -4,15 +4,20 @@ import org.happyxiaoanan.springbootmall.dao.UserDao;
 import org.happyxiaoanan.springbootmall.dto.UserRegisterRequest;
 import org.happyxiaoanan.springbootmall.model.User;
 import org.happyxiaoanan.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
-
 
     @Override
     public User getUserById(Integer userId) {
@@ -21,6 +26,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        // check email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("This email {} has been registered", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // create account
         return userDao.createUser(userRegisterRequest);
     }
 }
